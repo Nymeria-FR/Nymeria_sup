70f0cc5e2b7eee6cb515b7bb5278f0422770ba82
Valeran <valeran.maytie@protonmail.com>
2021-08-18 17:58:45 +0200

    add start embed

--- nymeria_sup.py
+++ nymeria_sup.py
@@ -37,18 +37,9 @@ async def on_ready():
 
 def member_in_cat(member):
     for channel in cat.channels:
-        if channel.name == member.name.lower() + "_" + str(member.id):
+        if channel.topic == str(member.id):
             return channel.id
     return None
-
-
-def take_id(name):
-    id = str()
-    for i in range(len(name) - 1, 0, -1):
-        if(name[i] == "_"):
-            break
-        id = name[i] + id
-    return int(id)
 
 
 @client.event
@@ -65,8 +56,12 @@ async def on_message(message):
                 if time.time() - chrono < 5:
                     await message.author.send("Le service est surchargé veuillez ressayer plus tard")
                 else:
-                    name = message.author.name + "_" + str(message.author.id)
+                    name = message.author.name + "-mp"
                     channel = await cat.create_text_channel(name)
+                    await channel.edit(topic=str(message.author.id))
+                    new = discord.Embed(title="Un member demande de l'aide",
+                                        description=f"Mention: {message.author.mention}\nUtilisateur: {message.author.name}\n ID: {message.author}")
+                    await channel.send(embed=new)
                     await channel.send(message.content)
                     chrono = time.time()
         elif(message.channel.category.id == cat.id and
@@ -74,7 +69,7 @@ async def on_message(message):
             if message.content == "close":
                 await message.channel.delete()
             else:
-                id = take_id(message.channel.name)
+                id = int(message.channel.topic)
                 user = await client.fetch_user(id)
                 await user.send(message.content)
 
